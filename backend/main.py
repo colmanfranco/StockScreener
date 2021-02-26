497508fc964588064ac6e5ef9818660a1c0982dc
Franco Colman <colmanfm@gmail.com>
2021-02-26 15:07:09 +0100

    [ADD] Added candlestick to the router and introduced a base_router to have just one instance of APIRouter

--- backend/main.py
+++ backend/main.py
@@ -1,9 +1,14 @@
+""" Main file for our FastAPI application """
 from fastapi import FastAPI
-from router import stock
+import uvicorn
+from router import stock, candlestick
 import model.Stock as StockModel
-from database.sqlite import engine, SessionLocal, Base
+from database.sqlite import engine
 
 app = FastAPI()
 StockModel.Base.metadata.create_all(bind=engine)
 
 app.include_router(stock.router, prefix='/api/v1')
+app.include_router(candlestick.router, prefix='/api/v1')
+if __name__ == "__main__":
+    uvicorn.run(app)
--- backend/router/stock.py
+++ backend/router/stock.py
@@ -1,22 +1,17 @@
-from fastapi import APIRouter, HTTPException, Request, Depends, BackgroundTasks
-from fastapi.templating import Jinja2Templates
+""" Router for stocks use case """
+import yfinance
+from fastapi import HTTPException, Request, Depends, BackgroundTasks
+from sqlalchemy.orm import Session
+from router.base_router import router, templates
 from model.Stock import StockRequest
 from model.Stock import Stock
-from database.sqlite import get_database_connection, SessionLocal
-from sqlalchemy.orm import Session
-import yfinance
-
-router = APIRouter()
-templates = Jinja2Templates(directory="templates")
+from database.sqlite import get_database_connection
 
 
 @router.get("/")
-def get_dashboard(request: Request, db: Session = Depends(get_database_connection)):
-    """
-    Homepage url
-    """
-    print(request)
-    stocks = db.query(Stock).all()
+def get_home_page(request: Request, db_session: Session = Depends(get_database_connection)):
+    """ Homepage url """
+    stocks = db_session.query(Stock).all()
     return templates.TemplateResponse("home.html", {
         "request": request,
         "stocks": stocks,
@@ -26,14 +21,12 @@ def get_dashboard(request: Request, db: Session = Depends(get_database_connectio
 @router.post("/stock")
 async def create_stock(stock_request: StockRequest,
                        background_tasks: BackgroundTasks,
-                       db: Session = Depends(get_database_connection)):
-    """
-    Endpoint to create a now stock to track
-    """
+                       db_session: Session = Depends(get_database_connection)):
+    """ Endpoint to create a now stock to track """
     stock = Stock()
     stock.ticker = stock_request.ticker
-    db.add(stock)
-    db.commit()
+    db_session.add(stock)
+    db_session.commit()
 
     background_tasks.add_task(fetch_stock_data, stock.id)
     return {
@@ -43,8 +36,9 @@ async def create_stock(stock_request: StockRequest,
 
 
 def fetch_stock_data(stock_id: int):
-    db = get_database_connection()
-    stock = db.query(Stock).filter(Stock.id == stock_id).first()
+    """ Fetch stock data using yfinance """
+    db_session = get_database_connection()
+    stock = db_session.query(Stock).filter(Stock.id == stock_id).first()
     ticker_data = yfinance.Ticker(stock.ticker)
 
     stock.price = ticker_data.info['previousClose']
@@ -55,5 +49,5 @@ def fetch_stock_data(stock_id: int):
     if ticker_data.info['dividendYield'] is not None:
         stock.dividend_yield = ticker_data.info['dividendYield'] * 100
 
-    db.add(stock)
-    db.commit()
+    db_session.add(stock)
+    db_session.commit()
